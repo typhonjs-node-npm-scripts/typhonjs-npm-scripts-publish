11f6c6bd22210cc0eb0ca44f94b9d0953baeae43
typhonrt <support@typhonrt.org>
2016-02-25 01:31:50 -0800

    0.0.4 release

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,13 +1,13 @@
 'use strict';
 
 /**
- * prepublish -- Runs all scripts defined in `npm-publish.json` scripts entry, but only if pre-publish mode is true.
- * Node / NPM currently has a bug (https://github.com/npm/npm/issues/10074) that will run the `prepublish` script when
- * modules are installed or `npm pack` is executed. In these cases it is not desirable to run actual pre-publish
- * actions. NPM module `in-publish` provides pre-publish detection.
+ * prepublish -- Runs all scripts defined in `npm-scripts.json` `publish.prepublish.scripts` entry, but only if
+ * pre-publish mode is true. Node / NPM currently has a bug (https://github.com/npm/npm/issues/10074) that will run the
+ * `prepublish` script when modules are installed or `npm pack` is executed. In these cases it is not desirable to run
+ * actual pre-publish actions. NPM module `in-publish` provides pre-publish detection.
  *
- * `npm-publish.json` must be defined in the root path and contain an object hash named `prepublish` with the
- * following options:
+ * `npm-scripts.json` must be defined in the root path and contain an object hash `publish` with a `prepublish` hash
+ * with the following options:
  * ```
  * (Array<string>)   scripts - An array of executable actions / scripts.
  * ```
@@ -21,12 +21,12 @@ if (inPublish || process.env.NPM_IN_PUBLISH_TEST)
    var fs =                require('fs-extra');
    var stripJsonComments = require('strip-json-comments');
 
-   // Verify that `npm-publish.json` exists.
+   // Verify that `npm-scripts.json` exists.
    try
    {
-      if (!fs.statSync('./npm-publish.json').isFile())
+      if (!fs.statSync('./npm-scripts.json').isFile())
       {
-         throw new Error("'npm-publish.json' not found in root path.");
+         throw new Error("'npm-scripts.json' not found in root path.");
       }
    }
    catch(err)
@@ -34,35 +34,46 @@ if (inPublish || process.env.NPM_IN_PUBLISH_TEST)
       throw new Error("TyphonJS NPM script (prepublish) error: " + err);
    }
 
-   // Load `npm-publish.json` and strip comments.
-   var configInfo = JSON.parse(stripJsonComments(fs.readFileSync('./npm-publish.json', 'utf-8')));
+   // Load `npm-scripts.json` and strip comments.
+   var configInfo = JSON.parse(stripJsonComments(fs.readFileSync('./npm-scripts.json', 'utf-8')));
+
+   // Verify that publish entry is an object.
+   if (typeof configInfo.publish !== 'object')
+   {
+      throw new Error(
+       "TyphonJS NPM script (prepublish) error: publish entry is not an object or is missing in "
+       + "'npm-scripts.json'.");
+   }
 
    // Verify that prepublish entry is an object.
-   if (typeof configInfo.prepublish !== 'object')
+   if (typeof configInfo.publish.prepublish !== 'object')
    {
       throw new Error(
-       "TyphonJS NPM script (prepublish) error: prepublish entry is not an object or is missing in "
-        + "'npm-publish.json'.");
+       "TyphonJS NPM script (prepublish) error: publish.prepublish entry is not an object or is missing in "
+        + "'npm-scripts.json'.");
    }
+
+   var prepublishConfig = configInfo.publish.prepublish;
 
    // Verify scripts entry exists
-   if (typeof configInfo.prepublish.scripts === 'undefined')
+   if (typeof prepublishConfig.scripts === 'undefined')
    {
       throw new Error(
-       "TyphonJS NPM script (prepublish) error: prepublish scripts entry is missing in 'npm-publish.json'.");
+       "TyphonJS NPM script (prepublish) error: publish.prepublish.scripts entry is missing in 'npm-scripts.json'.");
    }
 
-   if (!Array.isArray(configInfo.prepublish.scripts))
+   if (!Array.isArray(prepublishConfig.scripts))
    {
       throw new Error(
-       "TyphonJS NPM script (prepublish) error: prepublish scripts entry is not an array in 'npm-publish.json'.");
+       "TyphonJS NPM script (prepublish) error: publish.prepublish.scripts entry is not an array in "
+        + "'npm-scripts.json'.");
    }
 
    // Execute scripts
-   for (var cntr = 0; cntr < configInfo.prepublish.scripts.length; cntr++)
+   for (var cntr = 0; cntr < prepublishConfig.scripts.length; cntr++)
    {
       // Build base execution command.
-      var exec = configInfo.prepublish.scripts[cntr];
+      var exec = prepublishConfig.scripts[cntr];
 
       // Notify what command is being executed then execute it.
       process.stdout.write('Prepublish executing: ' + exec + '\n');
